41fb43dd645baa11b765982028189fe4675dbd04
kohi <kohinoorrijal@gmail.com>
2026-09-12 19:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,126 @@
 function enterWorld() {
+
     const intro = document.getElementById("intro");
+
     intro.classList.add("hidden");
+
 }
 
 
 function openPlace(place) {
 
     const world = document.getElementById("world");
-    const homeRoom = document.getElementById("home-room");
+
+    const homeLetter = document.getElementById("home-letter");
 
     const popup = document.getElementById("popup");
+
     const title = document.getElementById("popup-title");
+
     const text = document.getElementById("popup-text");
 
 
     // HOME
+
     if (place === "home") {
+
         world.style.display = "none";
-        homeRoom.style.display = "block";
+
+        homeLetter.style.display = "block";
+
         return;
+
     }
 
 
     // FOREST
+
     if (place === "forest") {
+
         title.textContent = "THE FOREST";
 
         text.textContent =
             "Somewhere in here are memories I never want to forget.";
+
     }
 
 
     // OCEAN
+
     if (place === "ocean") {
+
         title.textContent = "THE OCEAN";
 
         text.textContent =
             "There are things floating around here that I've never said enough.";
+
     }
 
 
     // OBSERVATORY
+
     if (place === "observatory") {
+
         title.textContent = "OBSERVATORY";
 
         text.textContent =
             "Look closely. Some of the stars are made from our memories.";
+
     }
 
 
     // FUTURE
+
     if (place === "future") {
+
         title.textContent = "???";
 
         text.textContent =
             "You haven't unlocked this place yet.";
+
     }
 
 
     popup.style.display = "flex";
+
 }
 
 
 function closePlace() {
+
     const popup = document.getElementById("popup");
 
     popup.style.display = "none";
+
 }
 
 
 function closeHome() {
 
     const world = document.getElementById("world");
-    const homeRoom = document.getElementById("home-room");
 
-    homeRoom.style.display = "none";
+    const homeLetter = document.getElementById("home-letter");
+
+    homeLetter.style.display = "none";
+
     world.style.display = "block";
+
+}
+
+
+function openLetter() {
+
+    const letter = document.getElementById("letter-content");
+
+    letter.style.display = "block";
+
+}
+
+
+function closeLetter() {
+
+    const letter = document.getElementById("letter-content");
+
+    letter.style.display = "none";
+
 }
